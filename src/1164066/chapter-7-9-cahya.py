251a217c05ad48a6b34ae9c6e2a494f3054dedd4
annisacahyani <annisaacahyanii@gmail.com>
2019-04-13 00:46:35 +0700

    CHAPTER KE-7

--- src/1164066/chapter-7-9-cahya.py
+++ src/1164066/chapter-7-9-cahya.py
@@ -1,13 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 10 20:41:37 2019
 
-@author: ASUS
-"""
-
-model = Sequential ()
-model.add ( Dense (512, input_shape = (2000,)))
-model.add ( Activation ('relu'))
-model.add ( Droupout (0.5))
-model.add ( Dense (2))
-model.add ( Activation ('softmax'))
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
